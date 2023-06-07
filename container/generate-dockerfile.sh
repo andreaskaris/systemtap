960481f3ffdd025359a692484bab7f92e267797f
Andreas Karis <ak.karis@gmail.com>
2023-06-07 11:57:26 +0200

    Improvements
    Signed-off-by: Andreas Karis <ak.karis@gmail.com>

--- container/generate-dockerfile.sh
+++ container/generate-dockerfile.sh
@@ -2,13 +2,13 @@
 
 set -eu
 
-KERNEL_VERSION="$1"
+KERNEL_VERSION="${1-}"
 if [ "${KERNEL_VERSION}" == "" ]; then
     echo "Please provide a kernel version (output of uname -r)"
     exit 1
 fi
-REGISTRY="$2"
-if [ "${REGISTRY}" == "" ]; then
+CONTAINER_IMAGE="${2-}"
+if [ "${CONTAINER_IMAGE}" == "" ]; then
     echo "Please provide a valid container image name"
     exit 1
 fi
@@ -20,4 +20,4 @@ RUN  yum install --enablerepo=rhel-8-for-x86_64-baseos-rpms --enablerepo=rhel-8-
      systemtap gcc kernel-devel-${KERNEL_VERSION} kernel-core-${KERNEL_VERSION} kernel-headers-${KERNEL_VERSION} \
      kernel-debuginfo-${KERNEL_VERSION} -y && yum clean all
 EOF
-podman build -t "${REGISTRY}" -f "Dockerfile.${KERNEL_VERSION}"
+podman build -t "${CONTAINER_IMAGE}" -f "Dockerfile.${KERNEL_VERSION}"

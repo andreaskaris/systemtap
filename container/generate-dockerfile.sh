14102faee12b3667edc68690b31195dd0f67055b
Andreas Karis <ak.karis@gmail.com>
2023-06-07 12:09:28 +0200

    Improvements
    Signed-off-by: Andreas Karis <ak.karis@gmail.com>

--- container/generate-dockerfile.sh
+++ container/generate-dockerfile.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
 set -eu
 
 KERNEL_VERSION="${1-}"
@@ -13,12 +15,15 @@ if [ "${CONTAINER_IMAGE}" == "" ]; then
     exit 1
 fi
 
-cat <<EOF > "Dockerfile.${KERNEL_VERSION}"
+cat <<EOF > "${DIR}/_output/Dockerfile.${KERNEL_VERSION}"
 FROM registry.redhat.io/rhel8/support-tools
 RUN  yum install --enablerepo=rhel-8-for-x86_64-baseos-rpms --enablerepo=rhel-8-for-x86_64-appstream-rpms \
-     --enablerepo=rhel-8-for-x86_64-baseos-debug-rpms \
+     --enablerepo=rhel-8-for-x86_64-baseos-debug-rpms --enablerepo=rhel-8-for-x86_64-baseos-eus-rpms  \
+     --enablerepo=rhel-8-for-x86_64-baseos-eus-debug-rpms \
      systemtap gcc kernel-devel-${KERNEL_VERSION} kernel-core-${KERNEL_VERSION} kernel-headers-${KERNEL_VERSION} \
      kernel-debuginfo-${KERNEL_VERSION} -y && yum clean all
 EOF
+pushd "${DIR}"
 podman build -t "${CONTAINER_IMAGE}:${KERNEL_VERSION}" -f "Dockerfile.${KERNEL_VERSION}"
 podman push "${CONTAINER_IMAGE}:${KERNEL_VERSION}"
+popd
